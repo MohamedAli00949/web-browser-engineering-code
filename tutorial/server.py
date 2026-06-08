fe3a95d9499b31eb1a347a4ea21e9334552e3da5
MohamedAli00949 <76850143+MohamedAli00949@users.noreply.github.com>
2026-06-08 04:15:43 +0300

    feat(ch-10): initialize cookies at the guest book server

--- tutorial/server.py
+++ tutorial/server.py
@@ -1,5 +1,6 @@
 import socket
 import urllib
+import random
 
 s = socket.socket(
     family=socket.AF_INET,
@@ -10,6 +11,8 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 s.bind(('', 8000))
 s.listen()
+
+SESSIONS = {}
 
 def handle_connection(conx):
     req = conx.makefile("b")
@@ -24,24 +27,34 @@ def handle_connection(conx):
         header, value = line.split(":", 1)
         headers[header.casefold()] = value.strip()
 
+    if 'cookie' in headers:
+        token = headers['cookie'][len("token="):]
+    else:
+        token = str(random.random())[2:]
+
+    session = SESSIONS.setdefault(token, {})
+
     if 'content-length' in headers:
         length = int(headers['content-length'])
         body = req.read(length).decode('utf8')
     else:
         body = None
 
-    status, body = do_request(method, url, headers, body)
+    status, body = do_request(session, method, url, headers, body)
 
     response = "HTTP/1.0 {}\r\n".format(status)
-    response += "Content-Length: {}\r\n".format(
-        len(body.encode("utf8")))
+    response += "Content-Length: {}\r\n".format(len(body.encode("utf8")))
+
+    if 'cookie' not in headers:
+        response += "Set-Cookie: token={}\r\n".format(token)
+
     response += "\r\n" + body
     conx.send(response.encode('utf8'))
     conx.close()
 
 ENTRIES = [ 'Pavel was here' ]
 
-def do_request(method, url, headers, body):
+def do_request(session, method, url, headers, body):
     out = "<!doctype html>"
     for entry in ENTRIES:
         out += "<p>" + entry + "</p>"
@@ -52,10 +65,11 @@ def do_request(method, url, headers, body):
     out += "</form>"
 
     if method == "GET" and url == "/":
-        return "200 OK", show_comments(out)
+        return "200 OK", show_comments(session, out)
     elif method == "POST" and url == "/add":
         params = form_decode(body)
-        return "200 OK", add_entry(params, out)
+        add_entry(session, params, out)
+        return "200 OK", show_comments(session)
     elif method == "GET" and url == "/comment.js":
         with open("comment.js") as f:
             return "200 OK", f.read()
@@ -69,7 +83,7 @@ def do_request(method, url, headers, body):
     else:
         return "404 Not Found", not_found(url, method, out)
 
-def show_comments(out):
+def show_comments(session, out):
     # ...
     out += "<strong></strong>"
     out += "<script src=/comment.js></script>"
@@ -85,10 +99,10 @@ def form_decode(body):
         params[name] = value
     return params
 
-def add_entry(params, out):
+def add_entry(session, params, out):
     if 'guest' in params and len(params['guest']) <= 100:
         ENTRIES.append(params['guest'])
-    return show_comments(out)
+    return show_comments(session, out)
 
 def not_found(url, method, out):
     out = "<!doctype html>"
